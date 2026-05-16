a109aa37518f5a15ec98dba943055dbe28aa70a7
Devs of A&M, Co. <dev@aberumirai.com>
2026-05-16 02:46:23 -0400

    funcion duplicada 2

--- workers/worker.js
+++ workers/worker.js
@@ -83,10 +83,6 @@ function getTokenFromCookie(request) {
   return match ? match[1] : null;
 }
 
-function makeSessionCookie(token, maxAgeSecs = 7 * 24 * 3600) {
-  return `session=${token}; HttpOnly; Secure; SameSite=Strict; Path=/; Max-Age=${maxAgeSecs}`;
-}
-
 // Hash de contraseña usando PBKDF2 nativo
 async function hashPassword(password, salt) {
   const encoder = new TextEncoder();
