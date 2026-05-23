3370b3d4ddaa1f4f3201098a70cfbbcd37a325e9
Devs of A&M, Co. <dev@aberumirai.com>
2026-05-23 12:46:51 -0400

    segunda prueba

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mirai-ai-v146'; // 👈 Cambia esto en cada deploy
+const CACHE_NAME = 'mirai-ai-v147'; // 👈 Cambia esto en cada deploy
 
 // ─── Páginas HTML a precargar ────────────────────────────────────────────────
 const HTML_PAGES = [
