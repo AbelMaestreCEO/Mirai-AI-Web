3cac551776156e1a02e5df7d939c190eb7406cea
Devs of A&M, Co. <dev@aberumirai.com>
2026-05-27 18:19:28 -0400

    dieta cacheada

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mirai-ai-v168'; // 👈 Cambia esto en cada deploy
+const CACHE_NAME = 'mirai-ai-v169'; // 👈 Cambia esto en cada deploy
 
 // ─── Páginas HTML a precargar ────────────────────────────────────────────────
 const HTML_PAGES = [
@@ -29,6 +29,7 @@ const HTML_PAGES = [
   '/projects',
   '/task',
   '/code',
+  '/diet'
   // Agrega aquí el resto de tus páginas
 ];
 
