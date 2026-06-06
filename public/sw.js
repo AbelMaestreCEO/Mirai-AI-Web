e5db75de6d4cc18f82902403dff5919e707e5157
Devs of A&M, Co. <dev@aberumirai.com>
2026-06-05 23:32:47 -0400

    TR classroom

--- public/sw.js
+++ public/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'mirai-ai-v205'; // 👈 Cambia esto en cada deploy
+const CACHE_NAME = 'mirai-ai-v206'; // 👈 Cambia esto en cada deploy
 
 // ─── Páginas HTML a precargar ────────────────────────────────────────────────
 const HTML_PAGES = [
